55499c5e1eef4620307871ab189c6c45ec2779b1
Mike Hannon(code) <mjhannon@gmail.com>
2015-11-06 11:23:30 -0800

    missing delete

--- server.py
+++ server.py
@@ -10,18 +10,22 @@ team = [
     {'id': 5,'first_name': 'Kareem' , 'last_name': 'Abdul-Jabbar' , 'jersey': 33 }]
 #helper functions
 def find_player(id):
-    for player in team:
+    for player in session['team']:
         if player['id'] == id:
             return player
     return false
 
 @app.route('/')
 def index():
+    try:
+        session['team']
+    except:
+        session['team'] = team
     return render_template('/index.html')
 
 @app.route('/players', methods = ['GET']) #show us all the players (index)
 def index_players():
-    return render_template('/players/index.html',team = team)
+    return render_template('/players/index.html',team = session['team'])
 
 @app.route('/players/<int:player_id>', methods = ['GET']) #this will get a player from our player list and show him by ID.
 def show(player_id):
@@ -31,16 +35,19 @@ def show(player_id):
          #return ends the function :)
     return redirect('/')
 
-@app.route("/players/<int:player_id>/edit")
+@app.route('/players/<int:player_id>/edit')
+#this shows a page allowing us to edit
 def edit(player_id):
     player = find_player(player_id)
     if player:
          return render_template('/players/edit.html', player = player)
     return redirect('/players/'+str(player_id))
 
-@app.route("/players/<int:player_id>", methods = ["POST"])
+@app.route('/players/<int:player_id>', methods = ['POST'])
 def update(player_id):
     player = find_player(player_id)
+
+
     player['first_name'] = request.form['first_name']
     player['last_name'] = request.form['last_name']
     player['jersey'] = request.form['jersey']
@@ -49,6 +56,12 @@ def update(player_id):
         player['team'] = request.form['team']
     except:
         print "try and get a contract!"
+        #session['team'] == [{},{},{},{}]
+    for player_ses in session['team']:
+            #player = {}
+        if player_ses['id'] == player_id:
+            player_ses = player
+
     print player['first_name']
     print player['last_name']
     print player['jersey']
